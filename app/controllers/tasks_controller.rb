4119b1c135ed9843c8dd21cf3938f9e9f1ec581e
Thomas Sohet <sohet.Thomas@gmail.com>
2017-10-31 16:59:53 +0100

    Refactored

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,44 +1,43 @@
 class TasksController < ApplicationController
+before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
     @tasks = Task.all
   end
 
   def show
-    id = params[:id]
-    @task = Task.find(id)
   end
 
   def new
-    @task = Task.create()
+    @task = Task.new()
   end
 
   def create
-    name = params[:task][:name]
-    details = params[:task][:details]
-    task = Task.create(name: name, details: details)
+    task = Task.create(task_params)
     redirect_to tasks_path
   end
 
   def edit
+  end
+
+  def update
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def set_task
     id = params[:id]
     @task = Task.find(id)
   end
 
-  def update
-    id = params[:id]
-    task = Task.find(id)
-    name = params[:task][:name]
-    details = params[:task][:details]
-    task.update(name: name, details: details)
-    redirect_to task_path(task)
+  def task_params
+    params.require(:task).permit(:details, :name)
   end
-
-  def destroy
-    id = params[:id]
-    task = Task.find(id)
-    task.destroy
-    redirect_to tasks_path
-  end
-
 end
